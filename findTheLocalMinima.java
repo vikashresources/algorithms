2441642aaa75e097ed2e23b65398ab6e0ca151cc
vikash choudhari <vikash.choudhari07@gmail.com>
2019-12-24 12:20:19 +0530

    Fix for similar occurances

--- findTheLocalMinima.java
+++ findTheLocalMinima.java
@@ -11,16 +11,24 @@ public class findTheLocalMinima {
 		else if(mid > 0 && arr[mid-1] < arr[mid])
 			return findMinima(arr,low, (mid-1), n);
 		
-		else
+		else if(mid > 0 && arr[mid+1] < arr[mid])
 			return findMinima(arr,(mid + 1), high, n);
+		else
+			{System.out.print("doesn't contain local minima element...");
+		return -1;}
 		
 	}
 	
 	public static void main(String[] args) {
 		System.out.print("Finding local minima...");
 		int[] arr = {9, 6, 3, 14, 5, 7, 4};
+		int arr1[] = {1, 2, 3};
+		int arr2[] = {3, 2, 1};
+		int arr3[] = {3,3,3,3};
 		System.out.print(findTheLocalMinima.findMinima(arr, 0,arr.length-1, arr.length));
-
+		System.out.print(findTheLocalMinima.findMinima(arr1, 0,arr1.length-1, arr1.length));
+		System.out.print(findTheLocalMinima.findMinima(arr2, 0,arr2.length-1, arr2.length));
+		System.out.print(findTheLocalMinima.findMinima(arr3, 0,arr3.length-1, arr3.length));
 	}
 
 }
